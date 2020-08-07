e14d0b0d1e0e5ad9ea99bccca8736c1223ce6c41
FerisJumbo <colegartner1@student.isd423.org>
2020-08-06 22:39:41 -0500

    init commit

---
+++ src/com/ferisjumbo/neuralnet/X_train-data/ImgLoader.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+img_dir = (
+    "src/com/ferisjumbo/neuralnet/X_train-data/"  # The directory to the training data
+)
+
+
+def get_img_array(fileloc):
+    """
+    Gets the specified image from this folder and returns a single array of numbers from 0-255
+    based on the grayscale.
+
+    Args:
+        fileloc (string): src/com/ferisjumbo/neuralnet/X_train-data + fileloc
+
+    Returns:
+        array[]: 1,024 numbers to be exact
+    """
+    image = cv2.imread(img_dir + fileloc)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    data = []
+    for x in img:
+        for y in x:
+            data.append(y)
+    return data
---
+++ src/com/ferisjumbo/neuralnet/X_train-data/Training_Data.py
@@ -0,0 +1,6 @@
+import ImgLoader as imgL
+
+number_four_1_dir = "four1.png"
+number_four_1 = imgL.get_img_array(number_four_1_dir)
+
+print(number_four_1)
